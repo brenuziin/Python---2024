ec2f5f465499a8cf7a034905e6c59ba02d796ab3
brenuziin <161034670+brenuziin@users.noreply.github.com>
2024-05-03 12:23:19 +0000

    Primeiro Passos - - Python

---
+++ Tipos de dados/Olá mundo.py
@@ -0,0 +1,18 @@
+
+# Atribuindo valores
+
+nome: str = "Brenuzin"
+idade: int = 20
+peso: float =  62.500
+
+# Incluindo dados
+
+print(nome)
+print(idade)
+print(peso)
+
+# Formatando
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso}")
