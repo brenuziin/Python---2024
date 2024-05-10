a4a84b7af3d6674e57c27048f037af497f6baa99
brenuziin <161034670+brenuziin@users.noreply.github.com>
2024-05-10 13:52:26 +0000

    aa

---
+++ Tipos de dados/Atividades/Atividade - 5 Pares ou Impares.py
@@ -0,0 +1,5 @@
+import os
+os.system("cls || clear")
+
+for i in range(1,5):
+    numero = input("Digite seu {i}° Número: ")
---
+++ Tipos de dados/Atividades/Atividade - Ímpares.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls || clear")
+
+for i in range(1,21, 2):
+    print(f"numero Ímpares: {i} ")
+
+for i in range(2,21, 2):
+    print(f"numero Pares: {i} ")
