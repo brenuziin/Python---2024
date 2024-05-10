be23175838a1223c0d0f681cfa5d1d45e25ddee0
brenuziin <161034670+brenuziin@users.noreply.github.com>
2024-05-10 13:17:04 +0000

    aa

---
+++ Tipos de dados/Atividades/Atividade - Multiplicação.py
@@ -0,0 +1,5 @@
+import os
+os.system("cls || clear")
+
+for i in range(1,10):
+    print(f"5x1 = {i}")
---
+++ Tipos de dados/Ensino/Laço de Repetição.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls || clear")
+
+for i in range(1,6):
+    print(f"Breno {i}")
+
+for i in range(5,0,-1):
+    print(f"Neymar Bola de Ouro de 201{i}")
